0e75e3344eba62411ad8bcc809bc4f80e451b4f4
Gergő Gáspár <gaspar.gergo@gmail.com>
2024-05-28 22:36:41 +0200

    Rearrange and clean up make_pdf script

--- make_pdf.sh
+++ make_pdf.sh
@@ -6,23 +6,24 @@ source_file=$1
 references=$2
 reference_folder=$(dirname $references)
 
-#populating header with correct paths and concatenating it with source file in a temp file
-sed -e "s|%STYLESHEET%|$script_folder/sourcefiles/stylesheet.mom|" -e "s|%REF_FILE%|$script_folder/sourcefiles/extracts.mom|" -e "s|%REF_DB%|$script_folder/.temp_ref_db|" $script_folder/sourcefiles/header.mom | cat - $source_file $script_folder/sourcefiles/bibliography.mom > $script_folder/.temp_source.mom
-
-#creating the to_SC file, by patching in earlier small caps, and calling the small caps script on new author fields
+#--- FORMATTING REFERENCE DATABASE ---
+#patching previous small caps choices if present before choosing scmall caps if new names were added
 cp $references $reference_folder/to_sc
 if [[ -f $reference_folder/sc.patch ]]; then patch $reference_folder/to_sc $reference_folder/sc.patch; fi
-tools/choose-small-caps.sh $reference_folder/to_sc
+$script_folder/tools/choose-small-caps.sh $reference_folder/to_sc
 diff $references $reference_folder/to_sc > $reference_folder/sc.patch
 
-#running preconv, so refer can use the file with Hungarian chars as well, this temp file is substituted into the mom source file header+bibliography
+#running preconv, so refer can use the file with Hungarian letters as well
 preconv $reference_folder/to_sc > $script_folder/.temp_ref_db
-sed -i -e "s|%REF_DB%|$script_folder/.temp_ref_db|" $script_folder/.temp_source.mom 
 
-#before running groff, run the source file through a sed filter which looks for
-#references in the [@reference] format, and changes the format to something groff can understand
-#sed -e 's/\.REF \(.*\)/\\c\n\.REF\n\.\[\n\1\n\.\]\n\.REF\n/' $source_file 
-#groff -Tpdf -mom -P-e -k -R $source_file > document.pdf
+#--- FORMATTING SOURCE FILE ---
+#concatenating source file with header and bibliography
+cat $script_folder/sourcefiles/header.mom $source_file $script_folder/sourcefiles/bibliography.mom > $script_folder/.temp_source.mom
+
+#populating source file with correct paths for stylesheet, reference implementation, and reference database
+sed -i -e "s|%STYLESHEET%|$script_folder/sourcefiles/stylesheet.mom|" -e "s|%REF_FILE%|$script_folder/sourcefiles/extracts.mom|" -e "s|%REF_DB%|$script_folder/.temp_ref_db|" $script_folder/.temp_source.mom
+
+#--- COMPILATION AND CLEANUP ---
 pdfmom -P-e -k -R $script_folder/.temp_source.mom > document.pdf
 
 #cleanup
