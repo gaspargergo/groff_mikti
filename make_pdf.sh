df6c74603e76614a217a00c3ae8484baee79c7e3
Gergő Gáspár <gaspar.gergo@gmail.com>
2024-07-26 10:57:24 +0200

    Make script more portable, move author and title into source file

--- make_pdf.sh
+++ make_pdf.sh
@@ -10,6 +10,7 @@ reference_folder=$(dirname $references)
 #patching previous small caps choices if present before choosing scmall caps if new names were added
 cp $references $reference_folder/to_sc
 if [[ -f $reference_folder/sc.patch ]]; then patch $reference_folder/to_sc $reference_folder/sc.patch; fi
+#run the small caps position choosing script for new authors; if there is no need, it simply quits
 $script_folder/tools/choose-small-caps.sh $reference_folder/to_sc
 diff $references $reference_folder/to_sc > $reference_folder/sc.patch
 
@@ -17,11 +18,11 @@ diff $references $reference_folder/to_sc > $reference_folder/sc.patch
 preconv $reference_folder/to_sc > $script_folder/.temp_ref_db
 
 #--- FORMATTING SOURCE FILE ---
-#concatenating source file with header and bibliography
-cat $script_folder/sourcefiles/header.mom $source_file $script_folder/sourcefiles/bibliography.mom > $script_folder/.temp_source.mom
+#concatenating source file with ref_macros, ref_header, stylesheet and bibliography
+cat $script_folder/sourcefiles/extracts.mom $script_folder/sourcefiles/header.mom $script_folder/sourcefiles/stylesheet.mom $source_file $script_folder/sourcefiles/bibliography.mom > $script_folder/.temp_source.mom
 
 #populating source file with correct paths for stylesheet, reference implementation, and reference database
-sed -i -e "s|%STYLESHEET%|$script_folder/sourcefiles/stylesheet.mom|" -e "s|%REF_FILE%|$script_folder/sourcefiles/extracts.mom|" -e "s|%REF_DB%|$script_folder/.temp_ref_db|" $script_folder/.temp_source.mom
+sed -i -e "s|%REF_DB%|$script_folder/.temp_ref_db|" $script_folder/.temp_source.mom
 
 #--- COMPILATION AND CLEANUP ---
 pdfmom -P-e -k -R $script_folder/.temp_source.mom > $3
